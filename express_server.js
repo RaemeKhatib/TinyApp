a51ed6aa1555fcb95e6672dbf7c67ff8a7a24ad1
Raeme Khatib <raeme.khatib@telus.com>
2022-11-28 14:45:06 -0700

    urls are now fully functional

--- express_server.js
+++ express_server.js
@@ -9,9 +9,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
