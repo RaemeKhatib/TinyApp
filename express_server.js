88444127f9bcbd984daaf2e53a31017d273417c2
Raeme Khatib <raeme.khatib@telus.com>
2022-11-28 18:32:22 -0700

    made sure that the url redirect, takes u to an active page

--- express_server.js
+++ express_server.js
@@ -2,17 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 const generateRandomString = () => {
   return ((Math.random() + 1)* 0x10000).toString(36).substring(6);
 }
-
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -23,37 +38,27 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const randomName = generateRandomString()
   const newLongUrl = req.body.longURL
-  urlDatabase[randomName] = newLongUrl
-  res.redirect(`/urls/${randomName}`) // Respond with 'Ok' (we will replace this)
+  if (newLongUrl.slice(0,8) === 'https://' || newLongUrl.slice(0,7) === 'http://') {
+    urlDatabase[randomName] = newLongUrl  // check if contains http: already
+  } else {
+    urlDatabase[randomName] = `https://${newLongUrl}`  // check if contains http: already
+  }
+    // check if contains http: already
+  res.redirect(`/urls/${randomName}`)
 });
 
 app.get("/urls/:id", (req, res) => {
+  const id = req.params.id
+  const longURL = urlDatabase[id]
+  const templateVars = { id, longURL};
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
   const id = req.params.id
   const longURL = urlDatabase[id]
   res.redirect(longURL);
