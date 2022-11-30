e5974a96f11631de184542511fd51c2348347bf2
Raeme Khatib <raeme.khatib@telus.com>
2022-11-30 16:12:56 -0700

    register new users now and with conditionals

--- express_server.js
+++ express_server.js
@@ -6,19 +6,27 @@ const { application } = require("express");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
+const findUserByEmail = (email) => {
+  for (const user in users) {
+    if (email === users[user].email) {
+      return user;
+    }
+  }
+  return null;
+};
 
 const generateRandomString = () => {
-  return ((Math.random() + 1)* 0x10000).toString(36).substring(6);
-}
+  return ((Math.random() + 1) * 0x10000).toString(36).substring(6);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const randomName = generateRandomString(); 
+const randomName = generateRandomString();
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -30,7 +38,7 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-funk",
   },
-}
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -48,39 +56,38 @@ app.listen(PORT, () => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
-  res.render("urls_index", templateVars );
+  const templateVars = { urls: urlDatabase, user:users[req.cookies["username"]] };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user:users[req.cookies["username"]] };
   res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  const randomName = generateRandomString()
-  const newLongUrl = req.body.longURL
-  if (newLongUrl.slice(0,8) === 'https://' || newLongUrl.slice(0,7) === 'http://') {
-    urlDatabase[randomName] = newLongUrl  // check if contains http: already
+ // Log the POST request body to the console
+  const randomName = generateRandomString();
+  const newLongUrl = req.body.longURL;
+  if (newLongUrl.slice(0, 8) === 'https://' || newLongUrl.slice(0, 7) === 'http://') {
+    urlDatabase[randomName] = newLongUrl;  // check if contains http: already
   } else {
-    urlDatabase[randomName] = `https://${newLongUrl}`  // check if contains http: already
+    urlDatabase[randomName] = `https://${newLongUrl}`;  // check if contains http: already
   }
-    // check if contains http: already
-  res.redirect(`/urls/${randomName}`)
+  // check if contains http: already
+  res.redirect(`/urls/${randomName}`);
 });
 
 app.get("/urls/:id", (req, res) => {   // redirect to  summary id page
-  const id = req.params.id
-  const longURL = urlDatabase[id]
-  const templateVars = { id, longURL, username:req.cookies["username"] };
-  res.render("urls_show", templateVars);
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { urls: urlDatabase, user:users[req.cookies["username"]] }; 
+   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {   // redirect to actual website
-  const id = req.params.id
-  const longURL = urlDatabase[id]
-  console.log(longURL)
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
 
@@ -92,8 +99,8 @@ app.post("/urls/:id/delete", (req, res) => {   // redirect to  summary id page
 
 app.post("/urls/:id/edit", (req, res) => {
   const shortUrl = req.params.id;
-  const newLongUrl = req.body.longUrl
-  
+  const newLongUrl = req.body.longUrl;
+
   if (newLongUrl.slice(0, 8) === 'https://' || newLongUrl.slice(0, 7) === 'http://') {
     urlDatabase[shortUrl] = newLongUrl;  // adds http: into input feild so http not manually required
   } else {
@@ -102,26 +109,44 @@ app.post("/urls/:id/edit", (req, res) => {
   res.redirect('/urls');
 });
 
-app.post("/login", (req, res) => {   
+app.post("/login", (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username)
-  res.redirect('/urls')
+  res.cookie('username', username);
+  res.redirect('/urls');
 });
 
-app.post("/logout", (req, res) => {   
-  res.clearCookie("username")
-  res.redirect('/urls')
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { username: null };
   res.render("urls_register", templateVars);
 });
 
-app.post("/register",(req, res) => { 
+app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  res.cookie('email', email)
-  res.cookie('password', password)
-  res.redirect('/register')
+  const user_id = randomName;
+
+  if (!email || !password) {
+    //respond with an error
+    res.send("error - please input email/password");
+  }
+  const foundUser = findUserByEmail(email);
+  if (foundUser) {
+    //respond with error email in use 
+    res.send("Email/password already exists");
+  } else {
+    const newUser = {
+      id: generateRandomString(),
+      email: email,
+      password: password
+    };
+    users[newUser.id] = newUser;
+    // console.log(users)
+    res.cookie('username', newUser.id);
+    res.redirect('/urls');
+  }
 });
