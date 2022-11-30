954fe90622d7fb73f0dc8e1c039c5a473f17a2c2
Raeme Khatib <raeme.khatib@telus.com>
2022-11-29 18:42:00 -0700

    now we can log out as function is now working with an if statement in header

--- express_server.js
+++ express_server.js
@@ -90,6 +90,10 @@ app.post("/urls/:id/edit", (req, res) => {
 app.post("/login", (req, res) => {   
   const username = req.body.username;
   res.cookie('username', username)
-  console.log(req.body)
+  res.redirect('/urls')
+});
+
+app.post("/logout", (req, res) => {   
+  res.clearCookie("username")
   res.redirect('/urls')
 });
